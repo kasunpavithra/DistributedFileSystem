55a2db8847a57fdd35fdec962fe6a1100c92feeb
kasunpavithra <mandalawattegekasunpavithra@gmail.com>
2024-04-04 15:15:26 +0530

    working

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,14 @@
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
     public static void main(String[] args){
         try{
-            Adder stub = (Adder) Naming.lookup("rmi://192.168.59.1:5000/adder");
+
+            Registry registry = LocateRegistry.getRegistry("192.168.8.100", 5000);
+
+            Adder stub = (Adder) registry.lookup("adder");
 
             System.out.println(stub.add(10,21));
 
--- src/Server.java
+++ src/Server.java
@@ -3,12 +3,12 @@ import java.rmi.registry.*;
 public class Server {
     public static void main(String[] args){
         try{
+            System.setProperty("java.rmi.server.hostname","192.168.8.100");
             Adder stub = new AdderRemote();
 
             Registry registry = LocateRegistry.createRegistry(5000);
 
-            System.setProperty("java.rmi.server.hostname","192.168.59.1");
-            Naming.rebind("rmi://192.168.59.1:5000/adder", stub);
+            registry.bind("adder", stub);
 
             System.out.println("Server is Running!");
         }catch(Exception e){
